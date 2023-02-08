adff6ff1dcbce3eda42126f9dfac6dfc4a622363
Wolfgang Senfter <w.senfter@gmail.com>
2023-02-08 20:40:36 +0100

    Fixed path

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
-    rust.url = "path: ./rust";
+    rust.url = "path:./rust";
   };
 
   outputs = inputs@{ nixpkgs, flake-utils, ... }:
@@ -10,13 +10,15 @@
       let
         pkgs = import nixpkgs { inherit system; };
         default-pkgs = pkgs;
-        mkNixShell = { pkgs ? default-pkgs, modules }: with pkgs;
-          mkShell (builtins.foldl' (config: module: config // { packages = config.packages ++ module.module.packages; } // module.module.env) { packages = [ ]; } modules);
+        mkNixShell = { pkgs ? default-pkgs, modules }:
+          with pkgs;
+          mkShell (builtins.foldl' (config: module:
+            config // {
+              packages = config.packages ++ module.module.packages;
+            } // module.module.env) { packages = [ ]; } modules);
         modules = [ inputs.rust ];
-      in
-      {
-        devShell.${system} =
-          mkNixShell { inherit modules; };
+      in {
+        devShell.${system} = mkNixShell { inherit modules; };
 
       });
 }
