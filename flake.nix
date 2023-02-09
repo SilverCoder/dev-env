c1571cf5dfebdbe4d7a32762b8fc4423a0d680fd
Wolfgang Senfter <w.senfter@gmail.com>
2023-02-09 13:05:46 +0100

    Fixes...

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    rust = { url = "path:./rust"; inputs.nixpkgs.follow = "nixpkgs"; };
+    rust = { url = "path:./rust"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { nixpkgs, rust, ... }: {
