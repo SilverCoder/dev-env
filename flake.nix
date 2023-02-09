fad4cc578649e64af09751f25f5b4a79338bdc98
Wolfgang Senfter <w.senfter@gmail.com>
2023-02-09 12:59:23 +0100

    Refactored to home-manager modules...

--- flake.nix
+++ flake.nix
@@ -1,31 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust.url = "path:./rust";
+    rust = { url = "path:./rust"; inputs.nixpkgs.follow = nixpkgs; };
   };
 
-  outputs = { nixpkgs, flake-utils, rust, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        default-pkgs = pkgs;
-        mkNixShell = { pkgs ? default-pkgs, modules }:
-          with pkgs;
-          mkShell (builtins.foldl'
-            (config: flake:
-              config // {
-                packages = config.packages ++ flake.module.packages;
-              } // (if builtins.hasAttr "env" flake.module then
-                flake.module.env
-              else
-                { }))
-            { packages = [ ]; }
-            modules);
-        modules = { inherit rust; };
-      in
-      {
-        devShell = mkNixShell { modules = (builtins.attrValues modules); };
-        inherit modules;
-      });
+  outputs = { nixpkgs, rust, ... }: {
+    homeManagerModules = {
+      rust = rust.homeManagerModules.default;
+    };
+  };
 }
--- rust/flake.nix
+++ rust/flake.nix
@@ -1,26 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, rust-overlay, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-
-        overlays = [ rust-overlay.overlays.default ];
-      };
-    in {
-      module = {
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
         packages = with pkgs; [
           rust-bin.stable.latest.default
           rust-bin.stable.latest.rust-analyzer
         ];
-      };
-    };
+      in
+      {
+        homeManagerModules =
+          let
+            module = { ... }: {
+              home.packages = packages;
+            };
+          in
+          {
+            default = module;
+            rust = module;
+          };
+
+        devShell = with pkgs; mkShell {
+          inherit packages;
+        };
+      });
 }
