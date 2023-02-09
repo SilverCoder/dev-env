0d5c05c5bd7ae14d600ab5d2792b3db1aac193b4
Wolfgang Senfter <w.senfter@gmail.com>
2023-02-09 22:59:31 +0100

    Readded flake-utils eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -1,12 +1,21 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
     rust = { url = "path:./rust"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { nixpkgs, rust, ... }: {
-    homeManagerModules = {
-      rust = rust.homeManagerModules.default;
-    };
-  };
+  outputs = { nixpkgs, flake-utils, rust, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          
+          config.allowUnfree = true;
+        };
+      in {
+        homeManagerModules = {
+          rust = rust.homeManagerModules.${system}.default;
+        };
+      });
 }
